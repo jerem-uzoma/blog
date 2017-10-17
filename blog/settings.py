9583d5b4ae7fcfcd9b09dbeedec954576c31cce8
Uzoma <nwokouzomaje@hotmail.com>
2017-10-17 14:46:04 +0100

    Some other, commit

--- blog/settings.py
+++ blog/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = SECRET_KEY = '8lu*6g0lg)9z!ba+a$ehk)xt)x%rxgb$i1&amp;022shmi1jcgihb
 DEBUG = False
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+        default=config'postgres://localhost'
+		)
 }
 
 ALLOWED_HOSTS = ['*']
